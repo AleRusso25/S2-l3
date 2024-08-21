6594b021e080b9a49464e7867b64d15a96c0a79a
Alessandra Russo <alerusso.2596@gmail.com>
2024-08-21 16:32:55 +0200

    add form and table

--- D3.js
+++ D3.js
@@ -170,6 +170,15 @@ console.log(eyeColor);
 /* ESERCIZIO 6
   Usa un while loop per calcolare la massa totale dell'equipaggio. Salvala in una variabile chiamata "crewMass".
 */
+let index = 0
+let crewMass = 0
+
+while (index < starWarsCharacters.length) {
+  const character = starWarsCharacters[index];
+  crewMass = crewMass + parseInt(character.mass);
+  index++;
+}
+console.log(crewMass)
 
 /* ESERCIZIO 7
   Crea uno if/else statement per rivelare la tipologia di carico, utilizzando la massa totale, di un'ipotetica astronave contenente i personaggi dell'array "starWarsCharacters".
